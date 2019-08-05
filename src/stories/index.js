4910440b6e65e689697dc27e4d9b4c54c3f01b35
Mustafa Saifee <saifee.mustafa@gmail.com>
2019-08-06 00:27:44 +0300

    Add cross section view

--- src/stories/index.js
+++ src/stories/index.js
@@ -34,6 +34,7 @@ import PointCloud from '../ReadMe/PointCloud.md';
 import ConnectedScatterPlot from '../ReadMe/ConnectedScatterPlot.md';
 import TreeMap from '../ReadMe/TreeMap.md';
 import TimeSeries from '../ReadMe/TimeSeries.md';
+import CrossSectionView from '../ReadMe/CrossSectionView.md';
 import SpiralPlot from '../ReadMe/SpiralPlot.md';
 import README from '../ReadMe/README.md';
 import coverImg from "../imgs/CoverImage.png";
@@ -3914,6 +3915,72 @@ storiesOf('TreeMap', module)
     />
   ))
 
+
+  storiesOf('Cross-section View', module)
+  .addWithJSX('Cross-section View', withReadme(CrossSectionView, () =>
+    <VRViz
+      scene={
+        {
+          'sky': {
+            'style': {
+              'color': '#333',
+              'texture': false,
+            }
+          },
+          'lights': [
+            {
+              'type': 'directional',
+              'color': '#fff',
+              'position': '0 1 1',
+              'intensity': 1,
+              "decay": 1,
+            },
+            {
+              'type': 'ambient',
+              'color': '#fff',
+              'intensity': 1,
+              "decay": 1,
+            }
+          ],
+          'camera': {
+            'position': '0 0 7.5',
+            'rotation': '0 0 0',
+          },
+          'reloadPageOnExitVR':true
+        }
+      }
+      graph={
+        [
+          {
+            'type': 'CrossSectionView',
+            'style': {
+              'position': [0, 3, 0],
+              'scale':[25,25,25],
+            },
+            'mark':{
+              'material':{
+                'type':'lambert',
+                'fill': {
+                  'opacity': 0.3,
+                  'color': '#ffffff'
+                },
+                'stroke': {
+                  'width': 1,
+                  'color': '#333',
+                  'edgeThresholdAngle':50
+                },
+                'highlightOnClick':{
+                  'opacity': 0.8,
+                  'color': '#ff0000',
+                }
+              },
+              'object':'data/gearbox_conical/scene.gltf'
+            }
+          }
+        ]
+      }
+    />
+  ))
 
 storiesOf('TimeSeries', module)
   .addWithJSX('TimeSeries', withReadme(TimeSeries, () =>
