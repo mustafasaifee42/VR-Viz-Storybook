88ec23db1c19fcef1f7f796c19577006536f3e4e
Mustafa Saifee <saifee.mustafa@gmail.com>
2019-09-02 17:17:05 +0300

    Update vr-viz version and add animated flow daigrams

--- src/stories/index.js
+++ src/stories/index.js
@@ -1727,6 +1727,123 @@ storiesOf('Maps', module)
       }
     />
   ))
+  .addWithJSX('Animated Flow Map', withReadme(FlowMap, () =>
+    <VRViz
+      scene={
+        {
+          'sky': {
+            'style': {
+              'color': '#333',
+              'texture': false,
+            }
+          },
+          'lights': [
+            {
+              'type': 'directional',
+              'color': '#fff',
+              'position': '0 1 1',
+              'intensity': 1,
+              "decay": 1,
+            },
+            {
+              'type': 'ambient',
+              'color': '#fff',
+              'intensity': 1,
+              "decay": 1,
+            }
+          ],
+          'camera': {
+            'position': '5 5 20',
+            'rotation': '0 0 0',
+          },
+          'reloadPageOnExitVR':true
+        }
+      }
+      graph={
+        [
+          {
+            'type': 'FlowMap',
+            'data': {
+              'dataFile': "data/data8.csv",
+              'fileType': 'csv',
+              'fieldDesc': [['source_latitude', 'number'], ['source_longitude', 'number'], ['target_latitude', 'number'], ['target_longitude', 'number']]
+            },
+            'style': {
+              'origin': [0, 0, 0],
+            },
+            'mark': {
+              'mapScale': 20,
+              'mapOrigin': [5, 5],
+              'rotation': '-90 0 0',
+              'map': {
+                'data': mapData,
+                'projection': 'Mercator',
+                'shapeIdentifier': 'id',
+                'shapeKey': 'countries',
+                'style': {
+                  'extrusion': {
+                    'value': 0.000001,
+                  },
+                  'fill': {
+                    'color': '#111',
+                    'opacity': 1,
+                  },
+                  'stroke': {
+                    'width': 1,
+                    'color': '#444',
+                  },
+                },
+              },
+              'flowlines': {
+                'style': {
+                  'opacity': 0.4,
+                  'stroke': {
+                    'color': '#2196f3',
+                  },
+                },
+                'flowAnimation':{
+                  'opacity':0.8,
+                  'color':'white',
+                  'duration':{
+                    "value":5000,
+                  },
+                  'radius':{
+                    "value":0.04,
+                  }
+                }
+              },
+              'nodes': {
+                'source': {
+                  'type': 'sphere',
+                  'style': {
+                    'radius': {
+                      'value': 0.05,
+                    },
+                    'fill': {
+                      'color': '#b71c1c',
+                      'opacity': 0.5,
+                    },
+                  }
+                },
+                'target': {
+                  'type': 'sphere',
+                  'style': {
+                    'radius': {
+                      'value': 0.05,
+                    },
+                    'fill': {
+                      'color': '#0f9d5b',
+                      'opacity': 0.5,
+                    },
+                  }
+                }
+              },
+            },
+          }
+        ]
+      }
+    />
+  ))
   .addWithJSX('Map With IsoLine (Contour Lines)', withReadme(MapWithIsoLines, () =>
     <VRViz
       scene={
@@ -3192,7 +3309,100 @@ storiesOf('Network Graph', module)
       }
     />
   ))
-
+  .addWithJSX('Animated Force Directed Graph', withReadme(ForceDirectedGraph, () =>
+    <VRViz
+      scene={
+        {
+          'sky': {
+            'style': {
+              'color': '#333',
+              'texture': false,
+            }
+          },
+          'lights': [
+            {
+              'type': 'directional',
+              'color': '#fff',
+              'position': '0 1 1',
+              'intensity': 1,
+              "decay": 1,
+            },
+            {
+              'type': 'ambient',
+              'color': '#fff',
+              'intensity': 1,
+              "decay": 1,
+            }
+          ],
+          'camera': {
+            'position': '0 5 10',
+            'rotation': '0 0 0',
+          },
+          'reloadPageOnExitVR':true
+        }
+      }
+      graph={
+        [
+          {
+            'type': 'ForceDirectedGraph',
+            'data': {
+              'dataFile': "data/ForceDirectedGraph.json",
+              'fileType': 'json',
+            },
+            'style': {
+              'origin': [0, 0, 0],
+              'scale': 0.2,
+            },
+            'mark': {
+              'nodes': {
+                'type': 'sphere',
+                'style': {
+                  'radius': {
+                    'value': 0.25,
+                  },
+                  'fill': {
+                    'scaleType': 'ordinal',
+                    'opacity': 1,
+                    'field': 'group',
+                  },
+                },
+              },
+              'links': {
+                'type': 'line',
+                'style': {
+                  'fill': {
+                    'opacity': {
+                      'value': 0.3,
+                    },
+                    'color': 'white',
+                  },
+                },
+                'flowAnimation':{
+                  'opacity':0.8,
+                  'color':'white',
+                  'duration':{
+                    "value":5000,
+                  },
+                  'radius':{
+                    "value":0.04,
+                  }
+                }
+              },
+              'labels': {
+                'field': 'id',
+                'style': {
+                  'color': 'white',
+                  'opacity': 1,
+                  'fontSize': 3,
+                  'padding': 0.1,
+                }
+              },
+            },
+          }
+        ]
+      }
+    />
+  ))
 
 storiesOf('Mesh Plot', module)
   .addWithJSX('Mesh Plot', withReadme(MeshPlot, () =>
